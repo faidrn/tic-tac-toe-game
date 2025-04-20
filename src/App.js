414ad0670e4b1cadcb269e0d575ad6cc305a65fd
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-04-20 23:55:45 +0200

    Add winner, showing when the game is won and there are no more turns to make

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null)); /* crea un arreglo de 9 elementos y establece cada elemento a null */
   const [xIsNext, setXIsNext] = useState(true);
 
+
   function handleClick(i){
-    if (squares[i]){
+    if (squares[i] || calculateWinner(squares)){
       // Si el cuadrado esta lleno, nos salimos de la función y no sobreescribimos en el cuadrado
       return;
     }
@@ -25,8 +26,19 @@ export default function Board() {
   }
 
 
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+
   return (
     <> {/* Los Fragments <></> envuelven multiples elementos JSX adyacentes */}
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square 
           value={squares[0]} 
@@ -71,4 +83,29 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+
+// Pongo la función al final para no hacer scroll cuando quiera editar el componente
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+
+  return null;
 }
