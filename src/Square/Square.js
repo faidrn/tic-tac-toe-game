8c1feea59c24602dcf50ef0b8070d052dd1a3c70
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-04-20 22:35:32 +0200

    Adding interactivity, when every square in the board is clicked

--- src/Square/Square.js
+++ src/Square/Square.js
@@ -1,5 +1,26 @@
-function Square({ value }){
-    return <button className="square">{value}</button>;
+import { useState } from "react";
+
+
+function Square(){
+    const [value, setValue] = useState(null); /* value=> almacena el valor, setValue=> es una función que puede ser usada para cambiar el valor  */
+        /* El valor null pasado a useState es usado como valor inicial para esta variable de estado */
+
+
+    function handleClick(){
+        setValue('X'); /* Se imprimira X cuando se haga click sobre el tablero */
+    }
+
+    return (
+        <button 
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
   }
 
   export { Square };
+
+  /* React provee una función llamada useState q se puede llamar desde el 
+    componente para dejarle recordar cosas  */
