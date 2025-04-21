27ba10a278d736325a6221ab7c087e202acfde70
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-04-21 18:40:21 +0200

    feat: Rewrite Board component to use two loops to make the squares instead of hardcoding them

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -28,53 +28,44 @@ function Board({ xIsNext, squares, onPlay }) {
     } else {
       status = "Next player: " + (xIsNext ? "X" : "O");
     }
+
+    // Usando loops para crear el Board
+    const boardSize = 3; // Tamaño del tablero
+
+    // Creación del tablero
+    const boardRows = [];
+
+    for (let row = 0; row < boardSize; row++){
+      const boardSquares = [];
+      for (let col = 0; col < boardSize; col ++){
+        const squareIndex = row * boardSize + col;
+        boardSquares.push(
+          <Square 
+            key={squareIndex}
+            value={squares[squareIndex]} 
+            onSquareClick={() => handleClick(squareIndex)}
+          />
+        );
+      }
+
+      boardRows.push(
+        <div 
+          key={row}
+          className="board-row"
+        >
+          {boardSquares}
+        </div>
+      );
+    }
   
   
     return (
       <> {/* Los Fragments <></> envuelven multiples elementos JSX adyacentes */}
-        <div className="status">{status}</div>
-        <div className="board-row">
-          <Square 
-            value={squares[0]} 
-            onSquareClick={() => handleClick(0)}
-        />
-          <Square 
-            value={squares[1]} 
-            onSquareClick={() => handleClick(1)}
-          />
-          <Square 
-            value={squares[2]} 
-            onSquareClick={() => handleClick(2)}
-          />
+        <div className="status">
+          {status}
         </div>
-        <div className="board-row">
-          <Square 
-            value={squares[3]} 
-            onSquareClick={() => handleClick(3)}
-          />
-          <Square 
-            value={squares[4]} 
-            onSquareClick={() => handleClick(4)}
-          />
-          <Square 
-            value={squares[5]} 
-            onSquareClick={() => handleClick(5)}
-          />
-        </div>
-        <div className="board-row">
-          <Square 
-            value={squares[6]} 
-            onSquareClick={() => handleClick(6)}
-          />
-          <Square 
-            value={squares[7]} 
-            onSquareClick={() => handleClick(7)}
-          />
-          <Square 
-            value={squares[8]} 
-            onSquareClick={() => handleClick(8)}
-          />
-        </div>
+
+        {boardRows}
       </>
     );
   }
