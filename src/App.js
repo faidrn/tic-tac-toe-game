8f01dc53354c88f3d7a50d94de0b512103e5c36a
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-04-21 12:37:55 +0200

    Adding new top-level component called Game to display a list of past moves

--- src/App.js
+++ src/App.js
@@ -1,111 +1,32 @@
 import { useState } from "react";
-import { Square } from "./Square/Square";
+import { Board } from "./Board/Board";
 
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null)); /* crea un arreglo de 9 elementos y establece cada elemento a null */
+export default function Games(){
   const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
 
-
-  function handleClick(i){
-    if (squares[i] || calculateWinner(squares)){
-      // Si el cuadrado esta lleno, nos salimos de la función y no sobreescribimos en el cuadrado
-      return;
-    }
-
-    const nextSquares = squares.slice();
-
-    if (xIsNext){
-      nextSquares[i] = "X";
-    } else{
-      nextSquares[i] = "O";
-    }
-    
-    setSquares(nextSquares);
+  function handlePlay(nextSquares){
+    setHistory([...history, nextSquares]); // Se crrea un array q contiene todos los items de history
     setXIsNext(!xIsNext);
   }
+  
 
-
-  const winner = calculateWinner(squares);
-  let status;
-
-  if (winner){
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
-
-  return (
-    <> {/* Los Fragments <></> envuelven multiples elementos JSX adyacentes */}
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square 
-          value={squares[0]} 
-          onSquareClick={() => handleClick(0)}
-      />
-        <Square 
-          value={squares[1]} 
-          onSquareClick={() => handleClick(1)}
-        />
-        <Square 
-          value={squares[2]} 
-          onSquareClick={() => handleClick(2)}
-        />
-      </div>
-      <div className="board-row">
-        <Square 
-          value={squares[3]} 
-          onSquareClick={() => handleClick(3)}
-        />
-        <Square 
-          value={squares[4]} 
-          onSquareClick={() => handleClick(4)}
-        />
-        <Square 
-          value={squares[5]} 
-          onSquareClick={() => handleClick(5)}
-        />
-      </div>
-      <div className="board-row">
-        <Square 
-          value={squares[6]} 
-          onSquareClick={() => handleClick(6)}
-        />
-        <Square 
-          value={squares[7]} 
-          onSquareClick={() => handleClick(7)}
-        />
-        <Square 
-          value={squares[8]} 
-          onSquareClick={() => handleClick(8)}
-        />
-      </div>
-    </>
-  );
-}
-
-
-// Pongo la función al final para no hacer scroll cuando quiera editar el componente
-function calculateWinner(squares){
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for (let i = 0; i < lines.length; i++){
-    const [a, b, c] = lines[i];
-
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-      return squares[a];
-    }
-  }
-
-  return null;
+    return(
+        <div className="game">
+            <div className="game-board">
+                <Board 
+                  xIsNext={xIsNext} 
+                  squares={currentSquares} 
+                  onPlay={handlePlay}
+                />
+            </div>
+            <div className="game-info">
+                <ol>
+                    {/* TODO */}
+                </ol>
+            </div>
+        </div>
+    );
 }
